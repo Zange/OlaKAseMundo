df40099a6bf41220653700fc2e623d21ab4d7d4a
Zange <alejandro.miralles@insbaixcamp.cat>
2013-02-26 16:15:28 +0100

    Mejorando el Java

--- OlaKAseMundo.java
+++ OlaKAseMundo.java
@@ -2,7 +2,11 @@ Class OlaKAseMundo{
 
 	public static main(String[] args){
 
-		System.out.println("Ola K Ase Mundo?");
-		System.out.println("Actulisa el JitJup O k Ase?");
+		try{
+			System.out.println("Ola K Ase Mundo?");
+			System.out.println("Actulisa el JitJup O k Ase?");
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 	}
 }
